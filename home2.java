81c6eac3cdee9f8964f9c5c906ec0b8ebd9a04ca
tensajougan8 <52632725+tensajougan8@users.noreply.github.com>
2019-07-16 23:50:39 +0530

    Add files via upload

---
+++ home2.java
@@ -0,0 +1,62 @@
+package lb4;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class home2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		Stack<Character> stk = new Stack();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter n number of values");
+	    int number = input.nextInt();
+	    int num=number+1;
+	    String a[]=new String[num];
+       for(i=0; i<num;i++)
+       	{
+           a[i] = input.nextLine();            
+       	}
+       char ch;
+       for(i=0; i<num;i++)
+       	{
+           String g=a[i]; 
+           OUTER_LOOP:
+           for(int j=0;j<g.length();j++)
+               {
+                    ch=g.charAt(j); 
+                    System.out.println("u"+ch);
+               
+		           if(ch=='{' || ch=='(' || ch=='[')
+		           	{
+		               stk.push(ch);     		               
+		           	}
+		           else
+		           {
+		        	   if(ch==']' && stk.peek()=='[' || ch=='}' && stk.peek()=='{' || ch==')' && stk.peek()=='(')
+		        	   {
+		        		   stk.pop();
+		        		   if(stk.isEmpty()) {
+		        			   System.out.println("balanced");
+		        			   
+		        		   }
+		        		   
+		        	   }
+		        	   else 
+		        	   {
+		        		   stk.clear(); 
+		        		   System.out.println("unbalanced");
+		        		   break OUTER_LOOP;
+		        	   }
+		        	   
+		           }
+		        	
+		           }
+           
+               }
+       	}
+	}
+
+
+
+
