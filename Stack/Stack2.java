377b349a51e6914451621edb7637a0dfd5e9f8bb
tensajougan8 <52632725+tensajougan8@users.noreply.github.com>
2019-09-02 12:11:47 +0530

    Update Stack2.java

--- Stack/Stack2.java
+++ Stack/Stack2.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//To find if the sequence is of the increasing or decreasing order using queue and stack
 package eva;
 
 import java.util.LinkedList;
