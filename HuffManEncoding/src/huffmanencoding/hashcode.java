53202d815ab1be6e90c44274b8d36e8c886da157
tensajougan8 <52632725+tensajougan8@users.noreply.github.com>
2019-09-16 12:21:58 +0530

    Add files via upload

---
+++ HuffManEncoding/src/huffmanencoding/hashcode.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package huffmanencoding;
+
+/**
+ *
+ * @author MScIT19
+ */
+public class hashcode
+{
+    int freq;
+    char c;
+    hashcode llink;
+    hashcode rlink;
+
+    public String ass = "";
+    hashcode()
+    {
+    }
+    hashcode(char a, int d)
+    {
+        c = a;
+        freq = d;
+       
+    }
+}
